a653dfcefdf0eeb4a944a6635788cbe94076a5d6
Aswin <Aswinjayadev6430@gmail.com>
2024-05-19 11:55:43 +0530

    database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,12 +97,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('NAME'),
+#         'USER': config('USER'),
+#         'PASSWORD': config('PASSWORD'),
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
+
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
+        'NAME': 'lit_insight',
+        'USER': 'lit_admin',
+        'PASSWORD': 'admin',
        'HOST': 'localhost',
        'PORT': '5432',
    }
